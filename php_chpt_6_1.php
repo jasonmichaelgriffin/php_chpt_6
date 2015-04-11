f0713693f7a8b68005bba5e10ce9f949120c7463
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-11 16:10:44 -0600

    Testing 6.12 of PHP book, playing around with the variables to get a deeper understanding.

--- php_chpt_6_1.php
+++ php_chpt_6_1.php
@@ -1,8 +1,9 @@
 <?php
 echo "<table style=\"border: 1px solid black;\"> \n";
-for ($y=1; $y<=20; $y++) {
+$y=1 ;
+for (; $y<=20; $y++) {
 	echo "<tr> \n";
-	for($x=1; $x <= 12; $x++) {
+	for($x=1; $x <= 20; $x++) {
 		echo "<td style=\"border: 1px solid #000; width:  25px;
 			text-align:center;\">";
 			echo ($x * $y);
