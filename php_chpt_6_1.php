0320699ac03008d8465557cda17bce06702cfb69
Jason Griffin <jasonmichaelgriffin@gmail.com>
2015-04-11 15:42:38 -0600

    Testing 6.12 of PHP book, playing around with the variables to get a deeper understanding.

--- php_chpt_6_1.php
+++ php_chpt_6_1.php
@@ -1,13 +1,15 @@
 <?php
-$counter = -4;
-for (; $counter <= 10; $counter++) {
-	if($counter == 0) {
-		break;
-	} else {
-		$temp = 4000 / $counter;
-		echo "4000 divided by " . $counter .  " is..." . $temp . "<br />";
+echo "<table style=\"border: 1px solid #000;\"> \n";
+for ($y=1; $y<=12; $y++) {
+	echo "<tr> \n";
+	for($x=1; $x <= 12; $x++) {
+		echo "<td style=\"border: 1px solid #000; width:  25px;
+			text-align:center;\">";
+			echo ($x * $y);
+			echo "</td> \n";
 		}
 }
+echo "</table>";
 ?>
 /**
 * Created by PhpStorm.
